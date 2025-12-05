e82d726f9a353071917a63d74d548bfb67326a6f
Alastor20 <nik_d.007@mail.ru>
2025-12-05 12:40:43 +0300

    p_t and f_t podik

--- main.cpp
+++ main.cpp
@@ -1,3 +1,38 @@
+#include <iostream>
+
+namespace topIT
+{
+    struct p_t
+    {
+        int x, y;
+    };
+    bool operator==(p_t, p_t);
+    bool operator!=(p_t, p_t);
+    struct f_t
+    {
+        p_t aa, bb;
+    };
+
+    struct IDraw
+    {
+        virtual p_t begin() const = 0;
+        virtual p_t next(p_t) const = 0;
+        virtual ~IDraw();
+    };
+}
 int main()
 {
+    using topIT::p_t;
+    p_t a{1, 0}, b{1, 0};
+    std::cout << (a == b) << '\n';
+}
+
+bool topIT::operator==(p_t a, p_t b)
+{
+    return a.x == b.x && a.y == b.y;
+}
+
+bool topIT::operator!=(p_t a, p_t b)
+{
+    return !(a == b);
 }
